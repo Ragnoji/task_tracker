5fe048e818012bc7a0cfe3edf929d31bd6200d46
Ragnoji <dok.hok@mail.ru>
2022-09-27 01:36:05 +0300

    Added UD of CRUD for Tasks; Implemented RESTful routes; Adapted all links to work with nested resources; Deadline is now forcefully set on 7-days after creation; Route for creation of task is bound to project, but you can still select other project inside form, might need to rethink it.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
+  before_action :set_project
   def index
-    @tasks = Task.all
+    @tasks = Task.where(project: @project)
   end
 
   def new
@@ -9,16 +11,42 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
+    @task.deadline_at = 7.days.after
 
     if @task.save
-      redirect_to @task
+      redirect_to project_task_url(@project, @task), notice: 'Task was successfully created!'
     else
+      flash[:notice] = 'Something went wrong. Try again.'
       render :new
     end
   end
 
+  def destroy
+    @task.destroy
+    redirect_to project_path(@task.project), notice: "Task was successfully destroyed."
+  end
+
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to project_task_url(@task.project, @task), notice: "Task was successfully updated."
+    else
+      flash[:notice] = "Something went wrong. Try again."
+      render :edit
+    end
+  end
+
+  def set_task
     @task = Task.find(params[:id])
+  end
+
+  def set_project
+    @project = Project.find(params[:project_id])
   end
 
   private
