5fe8b277f6886727012635076d95c22a0a4fae9b
Ragnoji <dok.hok@mail.ru>
2022-09-27 01:37:00 +0300

    Added validation for Task:  deadline > created_at case, presence of title, presence of project

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ApplicationRecord
   belongs_to :project
+  validates :project, presence: true
+  validates :title, presence: true
+  validate :deadline_after_creation
+
+  private
+  def deadline_after_creation
+    errors.add(:deadline_at, "must be after creation time") unless (!created_at.nil? and created_at < deadline_at) or Time.now.to_datetime < deadline_at
+  end
 end
