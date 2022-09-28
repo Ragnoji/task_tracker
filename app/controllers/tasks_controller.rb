df071b30d66e31de41df8e7c566ecc22cc509ce2
Ragnoji <dok.hok@mail.ru>
2022-09-28 21:45:35 +0300

    Changes according to reviewer's(Rasim) comments +flash.now +usage of has_many +find_by!

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,14 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[show edit update destroy]
-  before_action :set_project
+  before_action :set_project, only: %i[new index create destroy]
+
   def index
-    @tasks = Task.where(project: @project)
+    @tasks = @project.tasks
   end
 
   def new
+    # I create Task object here because view need to access errors coming from Task validations
+    # Later on it passes task: @task when rendering form inside tasks/new.html.erb
     @task = Task.new
   end
 
@@ -16,14 +19,14 @@ class TasksController < ApplicationController
     if @task.save
       redirect_to project_task_url(@project, @task), notice: 'Task was successfully created!'
     else
-      flash[:notice] = 'Something went wrong. Try again.'
+      flash.now[:notice] = 'Something went wrong. Try again.'
       render :new
     end
   end
 
   def destroy
     @task.destroy
-    redirect_to project_path(@task.project), notice: "Task was successfully destroyed."
+    redirect_to project_path(@project), notice: "Task was successfully destroyed."
   end
 
   def show
@@ -36,20 +39,20 @@ class TasksController < ApplicationController
     if @task.update(task_params)
       redirect_to project_task_url(@task.project, @task), notice: "Task was successfully updated."
     else
-      flash[:notice] = "Something went wrong. Try again."
+      flash.now[:notice] = "Something went wrong. Try again."
       render :edit
     end
   end
 
+  private
+
   def set_task
-    @task = Task.find(params[:id])
+    @task = Task.find_by!(id: params[:id])
   end
 
   def set_project
-    @project = Project.find(params[:project_id])
+    @project = Project.find_by!(id: params[:project_id])
   end
-
-  private
 
   def task_params
     params.require(:task).permit(:title, :description, :deadline_at, :project_id)
