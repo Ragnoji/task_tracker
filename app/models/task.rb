842632a1c732049b02790c21b4506838e71a93ca
Ragnoji <dok.hok@mail.ru>
2022-09-28 21:43:52 +0300

    Made validation's name more obvious

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,16 @@
 class Task < ApplicationRecord
   belongs_to :project
+
   validates :project, presence: true
   validates :title, presence: true
-  validate :deadline_after_creation
+  validate :deadline_later
 
   private
-  def deadline_after_creation
-    errors.add(:deadline_at, "must be after creation time") unless (!created_at.nil? and created_at < deadline_at) or Time.now.to_datetime < deadline_at
+
+  def deadline_later
+
+    unless (!created_at.nil? && created_at < deadline_at) || (created_at.nil? && Time.now.to_datetime < deadline_at)
+      errors.add(:deadline_at, "must be after creation time")
+    end
   end
 end
