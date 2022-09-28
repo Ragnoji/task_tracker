28c70e2a5a627e0a8dc675b0bf4aed2db79973fd
Ragnoji <dok.hok@mail.ru>
2022-09-28 18:09:05 +0300

    Revert to b1269ea43

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[show edit update destroy]
-  before_action :set_project
   def index
-    @tasks = Task.where(project: @project)
+    @tasks = Task.all
   end
 
   def new
@@ -11,42 +9,16 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-    @task.deadline_at = 7.days.after
 
     if @task.save
-      redirect_to project_task_url(@project, @task), notice: 'Task was successfully created!'
+      redirect_to @task
     else
-      flash[:notice] = 'Something went wrong. Try again.'
       render :new
     end
   end
 
-  def destroy
-    @task.destroy
-    redirect_to project_path(@task.project), notice: "Task was successfully destroyed."
-  end
-
   def show
-  end
-
-  def edit
-  end
-
-  def update
-    if @task.update(task_params)
-      redirect_to project_task_url(@task.project, @task), notice: "Task was successfully updated."
-    else
-      flash[:notice] = "Something went wrong. Try again."
-      render :edit
-    end
-  end
-
-  def set_task
     @task = Task.find(params[:id])
-  end
-
-  def set_project
-    @project = Project.find(params[:project_id])
   end
 
   private
