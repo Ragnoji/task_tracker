ad079ebc1aa5f832e91026b26c9aec4cd8c3d841
Ragnoji <dok.hok@mail.ru>
2022-09-29 14:11:18 +0300

    Removed nested if and improved statement itself

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,15 +1,15 @@
 class Task < ApplicationRecord
   belongs_to :project
 
-  validates :project, presence: true
   validates :title, presence: true
+  validates :deadline_at, presence: true
   validate :deadline_possibility
 
   private
 
   def deadline_possibility
-    unless (!created_at.nil? && created_at < deadline_at) || (created_at.nil? && Time.now.to_datetime < deadline_at)
-      errors.add(:deadline_at, "must be after creation time")
-    end
+    return if (created_at || Time.now.to_datetime) < deadline_at
+
+    errors.add(:deadline_at, "must be after creation time")
   end
 end
