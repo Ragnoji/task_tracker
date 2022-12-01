e11a92bafafbc4943fae08080e9a578beaf0a3af
Ragnoji <dok.hok@mail.ru>
2022-12-01 15:19:58 +0300

    added separate type for user role

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,7 +4,7 @@ module Types
     field :first_name, String
     field :last_name, String
     field :email, String
-    field :role, String
+    field :role, UserRoleEnum
     field :created_at, GraphQL::Types::ISO8601DateTime
     field :updated_at, GraphQL::Types::ISO8601DateTime
   end
