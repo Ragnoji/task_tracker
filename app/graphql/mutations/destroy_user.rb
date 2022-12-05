57ca14ba0c822be8ac5f5ada3bfcd706714c21b2
Ragnoji <dok.hok@mail.ru>
2022-12-05 13:50:04 +0300

    changed destroy_user mutation to destroy current_user

--- app/graphql/mutations/destroy_user.rb
+++ app/graphql/mutations/destroy_user.rb
@@ -2,14 +2,10 @@ module Mutations
   class DestroyUser < BaseMutation
     include GraphqlErrors
 
-    argument :input, Types::Inputs::DestroyUserInput, required: true
-
     type Types::Payloads::DestroyUserPayload
 
-    def resolve(input:)
-      user = User.find(input.id)
-
-      result = Users::Destroy.call(user: user)
+    def resolve
+      result = Users::Destroy.call(user: current_user)
 
       result.to_h.merge(errors: formatted_errors(result.user))
     end
--- app/graphql/types/inputs/destroy_user_input.rb
+++
@@ -1,7 +0,0 @@
-module Types
-  module Inputs
-    class DestroyUserInput < Types::BaseInputObject
-      argument :id, Integer, required: true
-    end
-  end
-end
