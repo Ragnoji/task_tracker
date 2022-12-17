d578a76d9de26a5b9d88897a292713cf313beae3
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2022-12-17 22:02:45 +0300

    Refactor

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,10 +4,11 @@ module Types
     field :sign_in, mutation: Mutations::SignIn
 
     field :create_project, mutation: Mutations::CreateProject
-    field :update_comment, mutation: Mutations::UpdateComment
     field :update_project, mutation: Mutations::UpdateProject
     field :destroy_project, mutation: Mutations::DestroyProject
 
     field :update_task, mutation: Mutations::UpdateTask
+    
+    field :update_comment, mutation: Mutations::UpdateComment
   end
 end
